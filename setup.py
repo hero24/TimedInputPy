4d79cb54d790d8b34f095be047f82728cb15a701
hero24 <hero24@interia.pl>
2019-08-30 19:24:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+args= {
+        "name" : "TimedInput",
+        "version" : "0.01",
+        "description" : "A library for handling timeout inputs",
+        "python_requires": ">=3.0",
+        "packages" : ["TimedInput"]
+}
+
+setup(**args)
